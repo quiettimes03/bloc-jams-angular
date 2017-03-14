729b166b2d4674b3924ab0bca0925d109eb84efd
quiettimes03 <Family@Renees-MacBook-Air.local>
2017-03-13 22:58:34 -0400

    Merge branch 'Checkpoint6'
    # Conflicts:
    #	app/scripts/controllers/AlbumCtrl.js
    #	app/templates/album.html
    #	dist/scripts/controllers/AlbumCtrl.js
    #	dist/templates/album.html

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,18 +1,7 @@
 (function(){
-<<<<<<< HEAD
-<<<<<<< HEAD
-    function AlbumCtrl(Fixtures){
-        this.albumData = Fixtures.getAlbum();
-=======
-    function AlbumCtrl(){
-        this.albumData = angular.copy(albumPicasso);
->>>>>>> Assignment5
-=======
     function AlbumCtrl(Fixtures, SongPlayer){
         this.albumData = Fixtures.getAlbum();
         this.songPlayer =  SongPlayer;
->>>>>>> Checkpoint7
-
     }
 
     angular
