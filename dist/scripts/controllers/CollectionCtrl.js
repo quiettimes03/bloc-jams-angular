a7e47987a4fdfc93d6724aa2f4f5bbc762c40ab9
quiettimes03 <Family@Renees-Air.westell.com>
2017-03-14 07:37:34 -0400

    Checkpoint7: completed

--- dist/scripts/controllers/CollectionCtrl.js
+++
@@ -1,9 +0,0 @@
-(function (){
-    function CollectionCrtl(Fixtures){
-        this.albums = Fixtures.getCollection(12);
-    }
-
-    angular
-      .module('blocJams')
-      .controller('CollectionCrtl', ['Fixtures', CollectionCrtl]);
-}) ();
