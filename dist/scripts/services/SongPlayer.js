709e3d45abcd6095bc93375f22f3ec5056845a5e
Renee Bain <rbain02@gmail.com>
2017-03-15 15:19:48 -0400

    revision

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,52 +1,22 @@
-<<<<<<< HEAD
-(function(){
-    function SongPlayer(){
-        var SongPlayer = {};
-
-        var currentSong = null;
-
-        /**
-        *@desc Buzz object audio form-inline
-        *@type {Object}
-        */
-        var currentBuzzObject = null;
-
-        /**
-        *@function setSong
-        *@desc Stops currently playing song and loads new audio file as currentBuzzObject
-        *@param {Object} song
-        */
-
-        var setSong = function(song){
-          if (currentBuzzObject){
-            currentBuzzObject.stop();
-            currentSong.playing = null;
-          }
-
-          currentBuzzObject = new buzz.sound(song.audioUrl, {
-            formats: ['mp3'],
-            preload: true
-          });
-
-          currentSong = song;
-        };
-
-        SongPlayer.play = function(song){
-          if (currentSong !== song){
-          setSong(song);
-          currentBuzzObject.play();
-          song.playing = true;
-        } else if (currentSong === song){
-            if (currentBuzzObject.isPaused()){
-              currentBuzzObject.play();
-=======
 (function() {
      function SongPlayer() {
           var SongPlayer = {};
 
+
           var currentSong = null;
+
+/**
+*@desc Buzz object audio file
+*@type {Object}
+*/
           var currentBuzzObject = null;
 
+
+/**
+*@function setSong
+*@desc Stops currently playing song and loads new audio file as currentBuzzObject
+*@param {Object} song
+*/
           var setSong = function(song){
               if (currentBuzzObject){
                   currentBuzzObject.stop();
@@ -70,7 +40,6 @@
                 if (currentBuzzObject.isPaused()){
                   currentBuzzObject.play();
                 }
->>>>>>> Checkpoint7
             }
           };
 
