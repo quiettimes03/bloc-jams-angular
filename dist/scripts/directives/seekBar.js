2cfccd10b6d33383dd85123b68824e8c9442ab09
Renee Bain <rbain02@gmail.com>
2017-03-23 15:12:31 -0400

    Merge branch 'Checkpoint10'
    # Conflicts:
    #	dist/scripts/directives/seekBar.js

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -14,18 +14,29 @@
           templateUrl: '/templates/directives/seek_bar.html',
           replace: true,
           restrict: 'E',
-          scope: { },
+          scope: {
+              onChange: '&'
+            },
           link: function(scope, element, attributes){
             scope.value = 0;
             scope.max = 100;
 
             var seekBar = $(element);
 
-            var percentString = function () {
-                var value = scope.value;
-                var max = scope.max;
-                var percent = value / max * 100;
-                return percent + "%";
+
+            attributes.$observe('value', function(newValue){
+                scope.value = newValue;
+            });
+
+            attributes.$observe('max', function(newValue){
+                scope.max = newValue;
+            });
+
+            var percentString = function(){
+              var value = scope.value;
+              var max = scope.max;
+              var percent = value / max * 100;
+              return percent + "%";
             };
 
             scope.fillStyle = function(){
@@ -33,26 +44,36 @@
             };
 
 
-            scope.onClickSeekBar = function(event) {
-                var percent = calculatePercent(seekBar, event);
-                scope.value = percent * scope.max;
+            scope.onClickSeekBar = function(event){
+              var percent = calculatePercent(seekBar, event);
+              scope.value = percent * scope.max;
+              notifyOnChange(scope.value);
+
             };
 
             scope.trackThumb = function() {
                 $document.bind('mousemove.thumb', function(event) {
-                var percent = calculatePercent(seekBar, event);
-                scope.$apply(function() {
-                scope.value = percent * scope.max;
-              });
-          });
+                  var percent = calculatePercent(seekBar, event);
+                  scope.$apply(function(){
+                      scope.value = percent * scope.max;
+                      notifyOnChange(scope.value);
+                  });
+                });
+                $document.bind('mouseup.thumb', function(){
+                    $document.unbind('mousemove.thumb');
+                    $document.unbind('mouseup.thumb');
+                });
 
-              $document.bind('mouseup.thumb', function() {
-                  $document.unbind('mousemove.thumb');
-                  $document.unbind('mouseup.thumb');
-              });
             };
+
+            var notifyOnChange = function(newValue){
+              if (typeof scope.onChange === 'function'){
+                scope.onChange({value: newValue});
+              }
+            };
+
             scope.thumbStyle = function(){
-              return {left: percentString()};
+              return{left: percentString()};
             };
           }
        };
